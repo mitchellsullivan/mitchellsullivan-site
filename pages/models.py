9cb43fcdc33fd43349c70bb3d91626798601f614
mitchellsullivan <jmsullivan@outlook.com>
2019-10-14 14:59:26 -0500

    initial draft of cv on home page. just get something up.

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    post_text = models.TextField()
+    post_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.post_text
+
--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,19 @@
 from django.views.generic import TemplateView
-
+from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 class HomePageView(TemplateView):
+    model = Post
     template_name = 'pages/home.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['posts'] = Post.objects.order_by('-post_date')[:1]
+        context['year'] = timezone.now().year
+        return context
 
 class AboutPageView(TemplateView):
     template_name = 'pages/about.html'
+
+
